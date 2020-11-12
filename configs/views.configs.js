802a51fab168f5d0f60fef13094f161ccc846686
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2020-11-12 10:01:22 +0100

    Removed server-favicon

--- configs/views.configs.js
+++ configs/views.configs.js
@@ -1,4 +1,3 @@
-const favicon = require('serve-favicon')
 const path = require('path')
 const express = require('express')
 
@@ -6,5 +5,4 @@ module.exports = app => {
     app.set('views', path.join(__dirname, '..', 'views'))
     app.set('view engine', 'hbs')
     app.use(express.static(path.join(__dirname, '..', 'public')))
-    app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
 }
