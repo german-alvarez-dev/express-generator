a363758ed40ab7f73410ec7daa76ad345b621533
german-alvarez-dev <german.alvarez@ironhack.com>
2021-02-16 12:39:42 +0100

    fixed typo

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     cleanText: text => text.trim(),
-    capitalizeText: text => text.charAt(0).toUpperCase + text.substring(1).toLowerCase()
+    capitalizeText: text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
 }
